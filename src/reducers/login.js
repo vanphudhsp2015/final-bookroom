e67eea8e854fef5f0ebcea39509153b8c93ad94a
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2019-07-12 07:54:33 +0700

    write logout if token by be remove

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -2,6 +2,7 @@ import * as types from '../constants/actionType';
 const INITIAL_STATE = {
   isLogin: false,
   error: null,
+  isToken: false,
   user: {}
 }
 export default function (state = INITIAL_STATE, action = {}) {
@@ -19,6 +20,10 @@ export default function (state = INITIAL_STATE, action = {}) {
     case types.REQUEST_LOGOUT:
       return Object.assign({}, state, {
         isLogin: false
+      })
+    case types.REQUEST_CHECK_TOKEN:
+      return Object.assign({}, state, {
+        isToken: true
       })
     default:
       return state;
